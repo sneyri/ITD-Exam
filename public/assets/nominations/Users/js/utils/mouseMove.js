5dc757de8c78dcd94cc9e604a4faacac104356ef
Sneyr <nikitoskonshin2009@gmail.com>
2026-05-06 19:14:30 +0300

    Revert "чзх"
    This reverts commit a68f9dddf645e76eb8ec8a5ff5d2b79d44bbb7ce.

--- public/assets/nominations/Users/js/utils/mouseMove.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-        --move-x: ${(e.clientX - window.innerWidth/2) * -0.008}deg;
-        --move-y: ${(e.clientY - window.innerHeight/2) * -0.025}deg;
-        `
-    })
-})
